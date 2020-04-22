24cce85756ed3dfc242210929e0cfe31a06c5bb3
JamesASchulze <james@jamesschulze>
2020-04-21 23:23:17 -0500

    Video 13: First Query & Mutation 20200421

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+   dogs(parent, args, ctx, info) {
+      global.dogs = global.dogs || [];
+      //return [{ name: 'Snickers'}, { name: 'Sonny'}];
+      return global.dogs;
+   },
+};
 
 module.exports = Query;
