7cd1d5ad32b3a6e48333be621051f32b5014e516
JamesASchulze <james@jamesschulze>
2020-04-23 23:54:51 -0500

    Video 14: Items Creation & Prisma Yoga Flow 20200423

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,11 +1,24 @@
 const mutations = {
-   createDog(parent, args, ctx, info) {
-      global.dogs = global.dogs || [];
-      // Create the dog!
-      const newDog = { name: args.name };
-      global.dogs.push(newDog);
-      return newDog;
+   async createItem(parent, args, ctx, info){
+      // TODO: check if they are logged in
+
+      const item = await ctx.db.mutation.createItem({
+         data: {
+            ...args
+         }
+      }, info);
+
+
+
+      return item;
    }
+   // createDog(parent, args, ctx, info) {
+   //    global.dogs = global.dogs || [];
+   //    // Create the dog!
+   //    const newDog = { name: args.name };
+   //    global.dogs.push(newDog);
+   //    return newDog;
+   // }
 };
 
 module.exports = mutations;
