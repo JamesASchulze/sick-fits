d24a0a9b268c746a89e0fcda1629c40aee15b2d0
James Schulze <James.Schulze@PlexsoftApps.com>
2021-03-22 22:56:47 -0500

    Video 12: Setting Up Keystone & TypeScript. 20210322 JS

---
+++ backend/keystone.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+    maxAge: 60 * 60 * 24 * 360,
+    secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true,
+        }
+    },
+    db: {
+        adapter: 'mongoose',
+        url: databaseURL,
+        // TODO: add data seeding here
+    },
+    lists: createSchema({
+        // Schema items go in here
+    }),
+    ui: {
+        // TODO: CHange this for roles
+        isAccessAllowed: () => true,
+    },
+    // TODO Add session values here.
+})
