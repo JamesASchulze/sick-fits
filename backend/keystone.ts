1df40034de58e8aabff8dd8c96ce7f3cd71992a2
James Schulze <James.Schulze@PlexsoftApps.com>
2021-07-06 23:07:25 -0500

    Video 18: Inserting Seed Data 20210706

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -5,6 +5,7 @@ import 'dotenv/config';
 import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -35,7 +36,12 @@ export default withAuth(
         db: {
             adapter: 'mongoose',
             url: databaseURL,
-            // TODO: add data seeding here
+            async onConnect(keystone) {
+                console.log("Connected to DB!");
+                if (process.argv.includes('--seed-data')) {
+                    await insertSeedData(keystone);
+                }
+            },
         },
         lists: createSchema({
             // Schema items go in here
