326c1461dd3b1ae6c0944d245d2d2321e582d0ab
James Schulze <James.Schulze@PlexsoftApps.com>
2021-07-12 23:01:31 -0500

    Video: 21 Fixing and Styling the Nav 20210712 JS

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,12 +1,13 @@
 import Link from 'next/link';
+import NaveStyles from './styles/NavStyles';
 
 export default function Nav() {
   return (
-    <nav>
+    <NaveStyles>
       <Link href="/products">Products</Link>
       <Link href="/sell">Sell</Link>
       <Link href="/orders">Orders</Link>
       <Link href="/account">Account</Link>
-    </nav>
+    </NaveStyles>
   );
 }
